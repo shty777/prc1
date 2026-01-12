560dca2ea057bb18027dabe55b32c0b905c1508a
shty777 <shentianyi7771210@163.com>
2026-01-12 14:34:54 +0000

    Initial commit

---
+++ Sequences/__init__.py
@@ -0,0 +1,6 @@
+# by importing and declaring a namespace here
+# to make it easier for users, it's common to 
+# make the namespace the same as the module name
+# I can simplify when importing into my script
+from . import Sequences as Sequences
+from . import Fasta as Fasta
